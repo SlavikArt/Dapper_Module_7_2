5b728cdcc4d6601a87adee21064ccdd5138ab56d
Vyacheslav <52282118+SlavikArt@users.noreply.github.com>
2024-06-12 12:18:04 +0200

    Null and Exception handling

--- Grocery/ProductService.cs
+++ Grocery/ProductService.cs
@@ -11,39 +11,60 @@ namespace Grocery
     {
         private SqlConnection connection;
 
-        public ProductService(SqlConnection connection) 
+        public ProductService(SqlConnection connection)
         {
             this.connection = connection;
         }
 
         public Product? GetProductById(int id)
         {
-            return connection.QueryFirstOrDefault<Product>("GetProductById", new { Id = id },
-                commandType: CommandType.StoredProcedure);
+            var product = connection.QueryFirstOrDefault<Product>("GetProductById", new { Id = id }, commandType: CommandType.StoredProcedure);
+            if (product == null)
+            {
+                throw new Exception("Продукт не знайдено");
+            }
+            return product;
         }
 
         public List<Product> GetAllProducts()
         {
-            return connection.Query<Product>("GetAllProducts", 
-                commandType: CommandType.StoredProcedure).ToList();
+            return connection.Query<Product>("GetAllProducts", commandType: CommandType.StoredProcedure).ToList();
         }
 
         public void UpdateProduct(Product product)
         {
-            connection.Execute("UpdateProduct", product, 
-                commandType: CommandType.StoredProcedure);
+            try
+            {
+                connection.Execute("UpdateProduct", product, commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Помилка при оновленні продукту", ex);
+            }
         }
 
         public void DeleteProduct(int id)
         {
-            connection.Execute("DeleteProduct", new { Id = id },
-                commandType: CommandType.StoredProcedure);
+            try
+            {
+                connection.Execute("DeleteProduct", new { Id = id }, commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Помилка при видаленні продукту", ex);
+            }
         }
 
         public double GetAverageCalories()
         {
-            return connection.Query<double>("GetAverageCalories",
-                commandType: CommandType.StoredProcedure).Single();
+            try
+            {
+                return connection.Query<double>("GetAverageCalories", commandType: CommandType.StoredProcedure).Single();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Помилка при обчисленні середньої калорійності", ex);
+            }
         }
     }
 }
